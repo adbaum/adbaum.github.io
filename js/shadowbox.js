f2daa145c16e52eba489bd5fbd1550f6a7546a6a
Adam Baumgartner <adam.f.baumgartner@gmail.com>
2014-09-16 23:20:37 -0400

    edit js

--- js/shadowbox.js
+++ js/shadowbox.js
@@ -1,7 +1,102 @@
-
-  $(".shadowbox").click(function(){
-    $(".shadowbox, .shadowbox2").fadeOut()
+$(".shadowbox").click(function(){
+    $(".shadowbox2").slideUp("linear");
+    $(".shadowbox").fadeOut("fast");
   });
-  $(".image").click(function(){
-    $(".shadowbox, .shadowbox2").fadeIn();
+
+  $(".button2").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button2").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button3").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button3").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button4").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button4").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button5").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button5").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button6").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button6").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button7").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button7").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button8").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button8").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button9").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button9").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button10").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button10").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button11").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button11").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button12").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button12").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button13").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button13").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button14").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button14").click(function(){
+    $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".button15").click(function(){
+    $(".shadowbox2").slideDown("linear");
+  });
+  $(".button15").click(function(){
+    $(".shadowbox").fadeIn("fast");
   });

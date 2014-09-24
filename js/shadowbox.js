3b654e3c4a95616cd5a9096eb44fe127e6566c5e
adbaum <itsadam.k@gmail.com>
2014-09-24 15:28:03 -0400

    JS duh

--- js/shadowbox.js
+++ js/shadowbox.js
@@ -268,10 +268,30 @@ $(".button14").click(function(){
     $(".shadowbox").fadeIn("fast");
   });
 
+  $(".shadowbox").click(function(){
+    $(".shadowbox18").slideUp("linear");
+    $(".shadowbox").fadeOut("fast");
+  });
+
+  $(".close-it").click(function(){
+    $(".shadowbox18").slideUp("linear");
+    $(".shadowbox").fadeOut("fast");
+  });
+
  $(".button17").click(function(){
     $(".shadowbox18").slideDown("linear");
   });
   $(".button17").click(function(){
     $(".shadowbox").fadeIn("fast");
+  });
+
+  $(".shadowbox").click(function(){
+    $(".shadowbox18").slideUp("linear");
+    $(".shadowbox").fadeOut("fast");
+  });
+
+  $(".close-it").click(function(){
+    $(".shadowbox18").slideUp("linear");
+    $(".shadowbox").fadeOut("fast");
   });
   });

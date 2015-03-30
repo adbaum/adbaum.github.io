0b372a638fd83b186a82b3c7ea5cfc68611fc934
Adam Baumgartner <adam.f.baumgartner@gmail.com>
2015-03-30 08:43:32 -0400

    will this work?

--- p/professor-garfield/scripts.js
+++ p/professor-garfield/scripts.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     $('.hover').bind('touchstart', function(e) {
         e;
-        $(this).addClass('hover_effect', function(f) {
-        	f;
-        	$(this).removeClass('hover_effect')
+        $(this).addClass('hover_effect', function() {
+        	$('.hover').bind('touchstart', function(f) {
+        		f;
+        		$(this).removeClass('hover_effect');
         });
     });
 });

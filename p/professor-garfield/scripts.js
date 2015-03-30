6999fab1aa8185c0cbb3fa3cd59c6fbd26eb6531
Adam Baumgartner <adam.f.baumgartner@gmail.com>
2015-03-30 08:36:44 -0400

    yes?

--- p/professor-garfield/scripts.js
+++ p/professor-garfield/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     $('.hover').bind('touchstart', function(e) {
         e;
-        $(this).toggleClass('hover_effect');
+        $(this).addClass('hover_effect', function(f) {
+        	f;
+        	$(this).removeClass('hover_effect')
+        });
     });
 });

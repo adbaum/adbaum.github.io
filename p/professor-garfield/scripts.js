ec27702e749a9b16a30c3a4aed21f0d4910a6ca3
Adam Baumgartner <adam.f.baumgartner@gmail.com>
2015-03-30 08:46:27 -0400

    to err is human

--- p/professor-garfield/scripts.js
+++ p/professor-garfield/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $('.hover').bind('touchstart', function(e) {
         e;
-        $(this).addClass('hover_effect', function() {
-        	$('.hover').bind('touchstart', function(f) {
-        		f;
-        		$(this).removeClass('hover_effect');
-        });
+        $(this).addClass('hover_effect',);
+    });
+    $('.hover').bind('touchstart', function(f) {
+        f;
+        $(this).removeClass('hover_effect',);
     });
 });

0e0e54faa69f93326442c9e28f1043e3728f222d
Adam Baumgartner <adam.f.baumgartner@gmail.com>
2017-08-29 00:12:00 -0700

    constrain

--- image-tilt/gyro-test/mobile.js
+++ image-tilt/gyro-test/mobile.js
@@ -7,7 +7,8 @@ window.addEventListener("deviceorientation", function(e) {
   var y = e.gamma;
 
 
-
+  if (x >  90) { x =  90};
+  if (x < -90) { x = -90};
 
   elem.style.transform =
     "rotateZ(" + ( z - 180 ) + "deg) " +

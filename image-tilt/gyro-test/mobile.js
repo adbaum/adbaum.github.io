c6935a05aa60f7c22a270da0d99d6f47764b3c17
Adam Baumgartner <adam.f.baumgartner@gmail.com>
2017-08-28 23:02:35 -0700

    back

--- image-tilt/gyro-test/mobile.js
+++ image-tilt/gyro-test/mobile.js
@@ -3,7 +3,7 @@ var elem = document.getElementById("view3d");
 window.addEventListener("deviceorientation", function(e) {
   // remember to use vendor-prefixed transform property
   elem.style.transform =
-    "rotateZ(" + ( -e.alpha ) + "deg) " +
-    "rotateX(" + ( -e.beta ) + "deg) " +
+    "rotateZ(" + ( e.alpha - 180 ) + "deg) " +
+    "rotateX(" + e.beta + "deg) " +
     "rotateY(" + ( -e.gamma ) + "deg)";
 });

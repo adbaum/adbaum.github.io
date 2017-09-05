e337e072d7e5237b763c6e7ac505827ddec47d26
Adam Baumgartner <adam.f.baumgartner@gmail.com>
2017-09-05 15:33:45 -0700

    this do it?

--- image-tilt/gyro-test/mobile.js
+++ image-tilt/gyro-test/mobile.js
@@ -11,6 +11,7 @@ window.addEventListener("deviceorientation", function(e) {
 
   if (y >  30) { y =  30};
   if (y <  -30) { y = -30};
+  if (x > 89 ) { y = (-1)*y }
 
   document.getElementById("z").innerHTML = z;
   document.getElementById("x").innerHTML = x;

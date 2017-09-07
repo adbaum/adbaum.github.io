86f2d3f4c8a976098fd30c00e7eed7e88a68aa01
Adam Baumgartner <adam.f.baumgartner@gmail.com>
2017-09-07 13:16:26 -0700

    order of operations?

--- image-tilt/gyro-test-two/mobile.js
+++ image-tilt/gyro-test-two/mobile.js
@@ -20,5 +20,5 @@ window.addEventListener("deviceorientation", function(e) {
     /*"rotateZ(" + ( z - 180 ) + "deg) " +*/
     /* "rotateX(" + ( x ) + "deg) " + */
     "rotateY(" + ( y ) + "deg)" +
-    "scaleX(" + ( y*1.01 ) + ")";
+    "scaleX(" + ( 1+y*.01 ) + ")";
 });

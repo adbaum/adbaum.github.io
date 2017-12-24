1a4411dae4d703d5e1e5814804c8a922658199bd
Adam Baumgartner <adam.f.baumgartner@gmail.com>
2017-12-24 15:24:53 -0500

    image

--- pickle/scripts.js
+++ pickle/scripts.js
@@ -1,10 +1,16 @@
 function PlaySound(soundobj) {
     var thissound=document.getElementById(soundobj);
     thissound.play();
+
+	var element = document.getElementById("bg");
+	element.classList.add("tickle-hand");
 }
 
 function StopSound(soundobj) {
     var thissound=document.getElementById(soundobj);
     thissound.pause();
     thissound.currentTime = 0;
+
+    var element = document.getElementById("bg");
+	element.classList.remove("tickle-hand");
 }
